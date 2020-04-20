5413472fa4a715b2c3f6b63d482883ebe20f463f
Tamas Konyves <40768809+tkonyves@users.noreply.github.com>
2020-04-20 19:29:18 +0200

    Fix Sonar issue java:S2119, reuse Random object

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,8 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  static Random generator = new Random();
+
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
@@ -35,9 +37,9 @@ public class TorpedoStore {
 
     boolean success = false;
 
+
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
-    double r = generator.nextDouble();
+    double r = TorpedoStore.generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
