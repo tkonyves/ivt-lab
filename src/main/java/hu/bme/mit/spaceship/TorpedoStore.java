8050b17ca8e91ded0e355d2c9be703ae208cb6b1
Tamas Konyves <40768809+tkonyves@users.noreply.github.com>
2020-04-20 19:40:28 +0200

    Fix Sonar issue java:S3984, throw the exception

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -32,11 +32,10 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
-
 
     // simulate random overheating of the launcher bay which prevents firing
     double r = TorpedoStore.generator.nextDouble();
