cbc12ea23cd939221f7231e7b1485e5d4201fb62
Tamas Konyves <40768809+tkonyves@users.noreply.github.com>
2020-04-22 10:48:14 +0200

    Feladat 2, Izolacio megvalositasa, Mockito tesztek

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,33 +8,42 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
+  private TorpedoStore mockTS;
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockTS = mock(TorpedoStore.class);
+
+    this.ship = new GT4500(mockTS, mockTS);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    // Set the behavior of the mock
+    when(mockTS.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(mockTS, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    // Set the behavior of the mock
+    when(mockTS.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(mockTS, times(2)).fire(1);
   }
 
 }
