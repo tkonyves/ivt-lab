4e3f62b2686b744755b076cd4484d002e3f0cd8a
Tamas Konyves <40768809+tkonyves@users.noreply.github.com>
2020-04-23 17:56:33 +0200

    Rename mockTS to mockTS1

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -12,42 +12,42 @@ import java.util.List;
 
 public class GT4500Test {
 
-  private TorpedoStore mockTS;
+  private TorpedoStore mockTS1;
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    mockTS = mock(TorpedoStore.class);
+    mockTS1 = mock(TorpedoStore.class);
 
-    this.ship = new GT4500(mockTS, mockTS);
+    this.ship = new GT4500(mockTS1, mockTS1);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
     // Set the behavior of the mock
-    when(mockTS.fire(1)).thenReturn(true);
+    when(mockTS1.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
-    verify(mockTS, times(1)).fire(1);
+    verify(mockTS1, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
     // Set the behavior of the mock
-    when(mockTS.fire(1)).thenReturn(true);
+    when(mockTS1.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
-    verify(mockTS, times(2)).fire(1);
+    verify(mockTS1, times(2)).fire(1);
   }
 
   public void multipleTorpedoFiring(int nr, FiringMode fm) {
@@ -62,13 +62,13 @@ public class GT4500Test {
       }
 
       int invocationTimes = fm == FiringMode.SINGLE ? nr : nr * 2;
-      verify(mockTS, times(invocationTimes)).fire(1);
+      verify(mockTS1, times(invocationTimes)).fire(1);
   }
 
   @Test
   public void fireTorpedo_Single_10_times() {
       // Arrange
-      when(mockTS.fire(1)).thenReturn(true);
+      when(mockTS1.fire(1)).thenReturn(true);
 
       // Act, Assert
       multipleTorpedoFiring(10, FiringMode.SINGLE);
@@ -77,7 +77,7 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_Single_100_times() {
       // Arrange
-      when(mockTS.fire(1)).thenReturn(true);
+      when(mockTS1.fire(1)).thenReturn(true);
 
       // Act, Assert
       multipleTorpedoFiring(100, FiringMode.SINGLE);
@@ -86,7 +86,7 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_10_times() {
       // Arrange
-      when(mockTS.fire(1)).thenReturn(true);
+      when(mockTS1.fire(1)).thenReturn(true);
 
       // Act, Assert
       multipleTorpedoFiring(10, FiringMode.ALL);
@@ -95,7 +95,7 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_100_times() {
       // Arrange
-      when(mockTS.fire(1)).thenReturn(true);
+      when(mockTS1.fire(1)).thenReturn(true);
 
       // Act, Assert
       multipleTorpedoFiring(100, FiringMode.ALL);
@@ -104,7 +104,7 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_AllAndSingle_alternate_100_times() {
     // Arrange
-    when(mockTS.fire(1)).thenReturn(true);
+    when(mockTS1.fire(1)).thenReturn(true);
 
     // Act
     int firingRounds = 100;
@@ -119,6 +119,6 @@ public class GT4500Test {
     for (int i = 0; i < results.size(); i++) {
       assertEquals(true, results.get(i));
     }
-    verify(mockTS, times(firingRounds * firesPerRound)).fire(1);
+    verify(mockTS1, times(firingRounds * firesPerRound)).fire(1);
   }
 }
